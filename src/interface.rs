fc3e44e25acfcef7845176eee775dddfe0ab6bba
Alexander van Saase <avsaase@gmail.com>
2024-05-05 00:36:39 +0200

    Convert to async API

--- src/interface.rs
+++ src/interface.rs
@@ -1,7 +1,7 @@
 //! I2C/SPI interfaces
 
 use crate::{private, Error};
-use embedded_hal::{i2c, spi::SpiDevice};
+use embedded_hal_async::{i2c, spi::SpiDevice};
 
 const I2C_DEV_BASE_ADDR: u8 = 0x68;
 
@@ -51,9 +51,9 @@ pub trait WriteData: private::Sealed {
     /// Error type
     type Error;
     /// Write to an u8 register
-    fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error>;
+    async fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error>;
     /// Write data. The first element corresponds to the starting address.
-    fn write_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error>;
+    async fn write_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error>;
 }
 
 impl<I2C, E> WriteData for I2cInterface<I2C>
@@ -61,15 +61,15 @@ where
     I2C: i2c::I2c<Error = E>,
 {
     type Error = Error<E>;
-    fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error> {
+    async fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error> {
         let payload: [u8; 2] = [register, data];
         let addr = self.address;
-        self.i2c.write(addr, &payload).map_err(Error::Comm)
+        self.i2c.write(addr, &payload).await.map_err(Error::Comm)
     }
 
-    fn write_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
+    async fn write_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
         let addr = self.address;
-        self.i2c.write(addr, payload).map_err(Error::Comm)
+        self.i2c.write(addr, payload).await.map_err(Error::Comm)
     }
 }
 
@@ -78,13 +78,13 @@ where
     SPI: SpiDevice<u8, Error = CommE>,
 {
     type Error = Error<CommE>;
-    fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error> {
+    async fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error> {
         let payload: [u8; 2] = [register, data];
-        self.spi.write(&payload).map_err(Error::Comm)
+        self.spi.write(&payload).await.map_err(Error::Comm)
     }
 
-    fn write_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
-        self.spi.write(payload).map_err(Error::Comm)
+    async fn write_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
+        self.spi.write(payload).await.map_err(Error::Comm)
     }
 }
 
@@ -93,9 +93,9 @@ pub trait ReadData: private::Sealed {
     /// Error type
     type Error;
     /// Read an u8 register
-    fn read_register(&mut self, register: u8) -> Result<u8, Self::Error>;
+    async fn read_register(&mut self, register: u8) -> Result<u8, Self::Error>;
     /// Read some data. The first element corresponds to the starting address.
-    fn read_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error>;
+    async fn read_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error>;
 }
 
 impl<I2C, E> ReadData for I2cInterface<I2C>
@@ -103,20 +103,22 @@ where
     I2C: i2c::I2c<Error = E>,
 {
     type Error = Error<E>;
-    fn read_register(&mut self, register: u8) -> Result<u8, Self::Error> {
+    async fn read_register(&mut self, register: u8) -> Result<u8, Self::Error> {
         let mut data = [0];
         let addr = self.address;
         self.i2c
             .write_read(addr, &[register], &mut data)
+            .await
             .map_err(Error::Comm)
             .and(Ok(data[0]))
     }
 
-    fn read_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
+    async fn read_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
         let len = payload.len();
         let addr = self.address;
         self.i2c
             .write_read(addr, &[payload[0]], &mut payload[1..len])
+            .await
             .map_err(Error::Comm)
     }
 }
@@ -126,15 +128,21 @@ where
     SPI: SpiDevice<u8, Error = CommE>,
 {
     type Error = Error<CommE>;
-    fn read_register(&mut self, register: u8) -> Result<u8, Self::Error> {
+    async fn read_register(&mut self, register: u8) -> Result<u8, Self::Error> {
         let mut data = [register + 0x80, 0];
-        self.spi.transfer_in_place(&mut data).map_err(Error::Comm)?;
+        self.spi
+            .transfer_in_place(&mut data)
+            .await
+            .map_err(Error::Comm)?;
         Ok(data[1])
     }
 
-    fn read_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
+    async fn read_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
         payload[0] += 0x80;
-        self.spi.transfer_in_place(payload).map_err(Error::Comm)?;
+        self.spi
+            .transfer_in_place(payload)
+            .await
+            .map_err(Error::Comm)?;
         Ok(())
     }
 }
